ffd8c4ec4deb0bec22ded61b6b003450e16cd5ec
Allan <allanl_95@hotmail.com>
2020-06-10 17:29:31 -0700

    Updates

--- hamburger-nav/src/components/Burger/index.js
+++
@@ -1,2 +0,0 @@
-// export the Burger folder so we can just import components folder
-export { default } from './Burger';
---
+++ precache-manifest.553473632859893dae8476fd8b282160.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9af4a2815a499e6038813461d7b84486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3921590fcbfe4f83616f",
+    "url": "/static/css/2.6c286d45.chunk.css"
+  },
+  {
+    "revision": "0c98705437d8b6665abf",
+    "url": "/static/css/main.9785e0ad.chunk.css"
+  },
+  {
+    "revision": "3921590fcbfe4f83616f",
+    "url": "/static/js/2.65e78e70.chunk.js"
+  },
+  {
+    "revision": "ddb934a41eb6bf13706c65fe3758fb10",
+    "url": "/static/js/2.65e78e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c98705437d8b6665abf",
+    "url": "/static/js/main.28f9f916.chunk.js"
+  },
+  {
+    "revision": "ffa79626e8f0fc782243",
+    "url": "/static/js/runtime-main.7c701b11.js"
+  },
+  {
+    "revision": "c6e35cc71f831689eac88cb8b812b3e3",
+    "url": "/static/media/allanliu_resume.c6e35cc7.pdf"
+  },
+  {
+    "revision": "864dba91ff41799f17ada7e1f2e6856c",
+    "url": "/static/media/architecture.864dba91.png"
+  },
+  {
+    "revision": "f8c9566d3b98c6a7a03aa03185afbfd7",
+    "url": "/static/media/dataviz.f8c9566d.png"
+  },
+  {
+    "revision": "619f1e1a37223a45a6d2518094155943",
+    "url": "/static/media/headshot.619f1e1a.jpg"
+  },
+  {
+    "revision": "6624c16ed680fc59528120f8b52b1b0b",
+    "url": "/static/media/sickfits.6624c16e.png"
+  }
+]);
