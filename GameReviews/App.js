18b61c6095a2467fb167a5870954bae0a4d563eb
Allan <allanl_95@hotmail.com>
2020-09-06 13:37:57 -0700

    to-do app p1

--- GameReviews/App.js
+++ GameReviews/App.js
@@ -1,63 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+// Components
+import Header from './components/header';
 
 const App = () => {
-  // const [name, setName] = useState('Allan');
-  // const [person, setPerson] = useState({
-  //   name: 'Mario',
-  //   age: 40
-  // });
 
-  // const handlePress = () => {
-  //   setName('Chun-li');
-  //   setPerson({
-  //     name: 'Luigi',
-  //     age: 45
-  //   });
-  // }
-
-  const [people, setPeople] = useState([
-      { name: 'shaun', id: '1' },
-      { name: 'test', id: '2' },
-      { name: 'mark', id: '3' },
-      { name: 'ben', id: '4' },
-      { name: 'ben', id: '5' },
-      { name: 'ben', id: '6' },
-      { name: 'ben', id: '7' },
-    ])
-
-  const pressHandler = (id) => {
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id)
-    })
-  }
+  const [todo, setTodo] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play games', key: '3' },
+    { text: 'ayy lmao', key: '4' },
+  ]);
 
   return (
     <View style={styles.container}>
-      {/*<ScrollView>
-      { people.map(item => (
-          <View key={item.key}>
-            <Text style={styles.item}>
-              {item.name}
-            </Text>
-          </View>
-      ))}
-      </ScrollView>*/}
-
-      <FlatList
-        keyExtractor={(item) => item.id}
-        data={people}
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>
-                {item.name}
-            </Text>
-          </TouchableOpacity>
-        )}
-        numColumns={2}
-      />
-
+      {/*Header*/}
+      <Header />
+      <View style={styles.content}>
+        {/*TODO Form*/}
+        <View style={styles.list}>
+          <FlatList 
+            data={todo}
+            renderItem={({item}) => (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -66,26 +36,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-    paddingTop: 40,
-    paddingHorizontal: 20,
   },
-  buttonContainer: {
-    marginTop: 20,
+  content: {
+    padding: 40,
   },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
-  item: {
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24
+  list: {
+    marginTop: 20
   }
 });
 
