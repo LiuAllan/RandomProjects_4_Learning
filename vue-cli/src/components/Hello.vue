9eb0fab310ffa61eca0bd698c71df004b2582cdf
Allan Liu <allan.liu95@gmail.com>
2020-10-05 15:56:03 -0700

    nested components

---
+++ vue-cli/src/components/Hello.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>List of Names</h1>
+        <ul>
+            <li v-for="(name, index) in names" :key="index">
+                {{ name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      names: [
+          'Yoshi',
+          'Mario',
+          'Ryu'
+      ]
+    }
+  },
+}
+</script>
+
+<style scoped>
+    div ul{
+       color: green;
+    }
+</style>
