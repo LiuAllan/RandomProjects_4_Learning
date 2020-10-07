d3daceb506d39b6c1cd07c0b356504ab45583adf
Allan Liu <allan.liu95@gmail.com>
2020-10-07 13:10:28 -0700

    blog input, checkboxes, and select options

---
+++ vue-blog/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <addBlog />
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue';
+
+export default {
+  name: 'App',
+  components: {
+    addBlog,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
